f6db8c2314345b9df91ac6a1618a85b11724320b
Michael Lee <michaellee@myndar.com>
2024-06-18 15:42:08 +0800

    dev

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,20 @@
-import PageNav from "./../components/PageNav";
-import { Link } from "react-router-dom";
+import styles from "./Homepage.module.css";
 
-function HomePage() {
-    return (
-        <div>
-            <PageNav />
-            <h1> WorldWise </h1>
-            <Link to="/app">Go to app</Link>
-        </div>
-    );
+export default function Homepage() {
+  return (
+    <main className={styles.homepage}>
+      <section>
+        <h1>
+          You travel the world.
+          <br />
+          WorldWise keeps track of your adventures.
+        </h1>
+        <h2>
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
+        </h2>
+      </section>
+    </main>
+  );
 }
-
-export default HomePage;
